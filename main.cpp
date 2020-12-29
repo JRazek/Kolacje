71fa00229c2c7ec3edbf0de3e4f1b7f644720ccc
Jakub <jakub.jakubecky123@gmail.com>
2020-12-29 13:15:30 +0100

    c

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+struct Node;
+
+struct Edge{
+    const int id;
+    const int cost;
+
+    Node * n1;
+    Node * n2;
+
+    Edge(int id, int cost, Node * n1, Node * n2):id(id), cost(cost), n1(n1), n2(n2){};
+};
 
 struct Node{
     const int id;
 
-    Node * parent;
-    vector<Node *> connections;
+    Node * parentPath;
+    vector<Edge *> connections;
+    const int restaurantType;
 
-    Node(int id): id(id){};
+    int lowOccurrence;
+    int highOccurrence;
+
+
+    Node(int id, int restaurantType): id(id), restaurantType(restaurantType){};
 };
 
 struct Query{
@@ -16,10 +32,42 @@ struct Query{
     const Node * n1;
     const Node * n2;
 
+    int low, high; //Mo's range
+
     Query(int id, Node * n1, Node * n2): id(id), n1(n1), n2(n2){};
 
     int answer;
+
+    struct Comparator{
+        const int blockSize;
+        Comparator(int blockSize, Query * q1, Query * q2): blockSize(blockSize){}
+        bool operator () (const Query * q1, Query * q2) const{
+            int lowIndexQ1, highIndexQ1;
+            lowIndexQ1 = (q1->n1->lowOccurrence > q1->n2->lowOccurrence ? q1->n1->lowOccurrence : q1->n2->lowOccurrence);
+            highIndexQ1 = (q1->n1->highOccurrence < q1->n2->highOccurrence ? q1->n1->highOccurrence : q1->n2->highOccurrence);
+            
+
+        }
+    };
+
 };
+
+void sortQueries(int blockSize, vector<Query *> &queries){
+
+}
+
+void dfs(Node * n, int comingFrom, vector<int> &occurrences){
+    occurrences.push_back(n->id);
+    n->lowOccurrence = occurrences.size() - 1;
+    for(auto e : n->connections){
+        if(e->id != comingFrom){
+            Node * another = (e->n1 == n ? e->n2 : e->n1);
+            dfs(another, e->id, occurrences);
+        }
+    }
+    occurrences.push_back(n->id);
+    n->highOccurrence = occurrences.size() - 1;
+}
 
 int main() {
     int nodesCount, typesCount;
@@ -27,17 +75,20 @@ int main() {
 
     vector<Node *> nodes;
     for(int i = 0; i < nodesCount; i ++){
-        nodes.push_back(new Node(i));
+        int type;
+        cin >> type;
+        type --;
+        nodes.push_back(new Node(i, type));
     }
     for(int i = 0; i < nodesCount - 1; i ++){
-        int n1ID, n2ID;
-        cin >> n1ID >> n2ID;
+        int n1ID, n2ID, cost;
+        cin >> n1ID >> n2ID >> cost;
         n1ID --; n2ID --;
         Node * n1 = nodes[n1ID];
         Node * n2 = nodes[n2ID];
-
-        n1->connections.push_back(n2);
-        n2->connections.push_back(n1);
+        Edge * e = new Edge(i, cost, n1, n2);
+        n1->connections.push_back(e);
+        n2->connections.push_back(e);
     }
 
     int queriesCount;
@@ -51,6 +102,14 @@ int main() {
 
         queries.push_back(new Query(i, nodes[n1], nodes[n2]));
     }
+
+    vector<int> dfsOrder; // each int corresponds to nodeID. first occurrence is preorder, second is postorder
+    dfs(nodes[0], -1, dfsOrder);
+    int blockSize = ceil(sqrt(dfsOrder.size()));
+
+    sortQueries(blockSize, queries);
+
+
 
 
 
